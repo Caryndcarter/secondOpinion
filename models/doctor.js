46aa2ec82ea4cd639961e96adbfc8e9cb2be37bb
ahang <hang.andy.t@gmail.com>
2017-05-11 14:46:54 -0500

    completed doctors/patients models

--- models/doctor.js
+++ models/doctor.js
@@ -1,6 +1,90 @@
 module.exports = function(sequelize, DataTypes) {
     var Doctors = sequelize.define("Doctors", {
-        name: DataTypes.STRING
-    });
+        bestdoc_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        primary_specialty: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        secondary_specialty: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                len: [1]
+            }
+        },
+        current_role_rating: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        pratice_rating: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        undergrad_rating: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        med_school_rating: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        residency_rating: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        fellowship_rating: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        publications_rating: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        years_exp_rating: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        clinical_trials_rating: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        patient_stars_rating: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        awards_rating: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        medicine_phD: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: 0
+        },
+        removed: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: 0
+        },
+    },
+        {
+            classMethods: {
+                associate: function(models) {
+                    Doctors.hasMany(models.Patients, {});
+                }
+            }
+        }
+    );
     return Doctors;
 };
