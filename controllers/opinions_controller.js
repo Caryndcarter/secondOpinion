6fc4663b8ca584d671b9a646470f9fe121a7dbb6
Laura McGinn <mcginn.laura@gmail.com>
2017-05-11 22:51:35 -0500

    post call adds user input to patient database

--- controllers/opinions_controller.js
+++ controllers/opinions_controller.js
@@ -5,12 +5,29 @@ var router = express.Router();
 var db = require("../models");
 
 router.get("/", function(req, res) {
-
     res.render("index");
 });
 
 router.post("/", function(req, res) {
-	console.log(req.body);
+	// console.log(req.body);
+	db.Patients.create({
+		name: req.body.patient_name,
+		email: req.body.email,
+		current_doctor: req.body.current_doctor,
+		diagnosis: req.body.diagnosis
+
+		console.log('patient created!');
+
+		//DOCTOR MATCH CALCULATION GOES HERE
+
+	}).then(function(dbPatients) {
+		//send the results of the doctor match as a response object
+		
+		// res.json(dbPatients);
+		
+
+
+	});
 });
 
 router.get("/admin", function(req, res) {
@@ -20,6 +37,9 @@ router.get("/admin", function(req, res) {
     	res.render("index", { burgers: results });
     });
     */
+
+
+
 });
 
 module.exports = router;
