d3ffbfa37f426b21f95baafc7d7aaf4feac29d91
ahang1 <hang.andy.t@gmail.com>
2017-05-13 13:24:29 -0500

    persistent login updated some routes in the auth controller

--- models/patient.js
+++ models/patient.js
@@ -1,11 +1,21 @@
 module.exports = function(sequelize, DataTypes) {
     var Patients = sequelize.define("Patients", {
-        name: {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        username: {
             type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
+            allowNull: false
+        },
+        firstname: {
+            type: DataTypes.STRING,
+            nontEmpty: true
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            nontEmpty: true
         },
         email: {
             type: DataTypes.STRING,
@@ -14,6 +24,10 @@ module.exports = function(sequelize, DataTypes) {
                 len: [1],
                 isEmail: true
             }
+        },
+        password: {
+            type: DataTypes.STRING,
+            notEmpty: true
         },
         current_doctor: {
             type: DataTypes.STRING,
@@ -37,9 +51,20 @@ module.exports = function(sequelize, DataTypes) {
                 len: [1]
             }
         },
+        last_login: {
+            type: DataTypes.DATE
+        },
+        isAdmin: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
         removed: {
             type: DataTypes.BOOLEAN,
             defaultValue: 0
+        },
+        status: {
+            type: DataTypes.ENUM("active", "inactive"),
+            defaultValue: "active"
         }
     },
         {
