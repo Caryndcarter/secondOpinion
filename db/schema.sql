6fe9d1834de0202c2ea73dfe2559ccf4216085e8
ahang <hang.andy.t@gmail.com>
2017-05-16 16:29:46 -0500

    updated schema.sql, updated style.css, and added CopyRight at the footer

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE second_opinion_db;  
+CREATE DATABASE second_opinion_db;
 
-USE second_opinion_db; 
+USE second_opinion_db;
 
 
 CREATE TABLE doctors(
@@ -20,19 +20,25 @@ CREATE TABLE doctors(
     years_exp_rating int(11) DEFAULT 0,
     clinical_trials_rating int(11) DEFAULT 0,
     patient_stars_rating int(11) DEFAULT 0,
-    awards_rating int(11) DEFAULT 0, 
-    total int(11) DEFAULT 0, 
+    awards_rating int(11) DEFAULT 0,
+    total int(11) DEFAULT 0,
     removed BOOLEAN DEFAULT 0,
     primary key(doc_id)
 )
 
 CREATE TABLE patients(
     patient_id int(11) NOT NULL AUTO_INCREMENT,
-    name varchar(50) NOT NULL,
+    username varchar(50) NOT NULL,
+    firstname varchar(50) NOT NULL,
+    lastname varchar(50) NOT NULL,
     email varchar(50) NOT NULL,
+    password varchar(50) NOT NULL,
     current_doctor varchar(50) DEFAULT NULL,
     match_doctor varchar(50) DEFAULT NULL,
-    diagnosis varchar(50) NOT NULL,
+    diagnosis varchar(50) DEFAULT NULL,
+    isAdmin BOOLEAN DEFAULT 0,
+    removed BOOLEAN DEFAULT 0,
+    last_login date(),
     removed BOOLEAN DEFAULT 0,
     primary key (patient_id)
 )
