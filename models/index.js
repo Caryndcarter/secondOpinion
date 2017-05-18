8a3f7bf709749d81bb8282d923142c224a40828c
ahang <hang.andy.t@gmail.com>
2017-05-18 11:38:51 -0500

    modified index.js

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize("postgres://hjprzuqlngyqrm:f99a91854181ec9707d206833eb054f3113b57fbaa6532fa290601fc3b2abd56@ec2-23-23-227-188.compute-1.amazonaws.com:5432/dalp2vne88k27h");
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
