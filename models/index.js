2c7484e655de6c2485925bdaade8b532dde4bf89
ahang <hang.andy.t@gmail.com>
2017-05-18 11:13:39 -0500

    persist

--- models/index.js
+++ models/index.js
@@ -7,23 +7,6 @@ var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
-var pg = require("pg");
-
-
-var pg = require('pg');
-
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
-
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize("postgres://hjprzuqlngyqrm:f99a91854181ec9707d206833eb054f3113b57fbaa6532fa290601fc3b2abd56@ec2-23-23-227-188.compute-1.amazonaws.com:5432/dalp2vne88k27h");
